584c3b497284512c00055dd5e3b70e569a92e52b
Mirza Ozan <mirza4ozan@gmail.com>
2024-12-08 00:41:11 +0300

    Yeni kod ekledim

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from['.header', {opactiy:0, duraction:1.5, delay:.5,y:50}]
+gsap.from['.text', {opactiy:0, duraction:1.5, delay:.5,y:50}]
+gsap.from['.img', {opactiy:0, duraction:1.5, delay:.5,y:50}]
+gsap.from['.section-1', {opactiy:0, duraction:1.5, delay:.5,y:50}]
